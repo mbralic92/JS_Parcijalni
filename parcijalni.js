43654a582c53415ac6dc7571bd4057c4c6016cc9
Neven <nevenmiocevic@gmail.com>
2023-10-11 20:38:36 +0200

    ispravak

--- parcijalni.js
+++ parcijalni.js
@@ -1,36 +1,57 @@
-  var searchInput = document.querySelector(".search");
-  searchInput.addEventListener("input", (e) => {
+var searchInput = document.querySelector(".search");
+var loader = document.querySelector("#loader");
+loader.style.display = "none"; // Loader skriven na početku
+
+searchInput.addEventListener("input", (e) => {
   const input = e.target.value;
-    
-              
-  const apiURL = `https://api.tvmaze.com/search/shows?q=${input}`;
 
-  fetch(apiURL)
-    .then((response) => response.json())
-    .then((data) => {
+  // Startaj bez poruke i bez loadera
+  const messageElement = document.querySelector("#poruka");
+  messageElement.innerHTML = "";
+  loader.style.display = "none";
 
-      const tbody = document.querySelector("#results tbody");
-      tbody.innerHTML = "";
+   setTimeout(() => {
+    const apiURL = `https://api.tvmaze.com/search/shows?q=${input}`;
 
-      data.forEach((element) => {
-        const row = document.createElement("tr");
+    // Pokazi loader animaciju u trenutku pretrage APIa
+    loader.style.display = "block";
 
-        const seriesElement = document.createElement("td");
-        seriesElement.innerText = element.show.name;
-        const ratingElement = document.createElement("td");
-        ratingElement.innerText = element.show.rating.average;
-        const genresElement = document.createElement("td");
-        genresElement.innerText = element.show.genres;
-        const descriptionElement = document.createElement("td");
-        descriptionElement.innerHTML = element.show.summary;
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((data) => {
+        const tbody = document.querySelector("#results tbody");
+        tbody.innerHTML = "";
 
-        row.appendChild(seriesElement);
-        row.appendChild(ratingElement);
-        row.appendChild(genresElement);
-        row.appendChild(descriptionElement);
+        if (data.length === 0) {
+          // Ako nema rezultata prikazi sljedeću poruku
+          messageElement.textContent = "Nema rezultata";
+        } else {
+          data.forEach((element) => {
+            const row = document.createElement("tr");
 
-        tbody.appendChild(row);
+            const seriesElement = document.createElement("td");
+            seriesElement.innerText = element.show.name;
+            const ratingElement = document.createElement("td");
+            ratingElement.innerText = element.show.rating.average;
+            const genresElement = document.createElement("td");
+            genresElement.innerText = element.show.genres;
+            const descriptionElement = document.createElement("td");
+            descriptionElement.innerHTML = element.show.summary;
+
+            row.appendChild(seriesElement);
+            row.appendChild(ratingElement);
+            row.appendChild(genresElement);
+            row.appendChild(descriptionElement);
+
+            tbody.appendChild(row);
+          });
+        }
+
+        // Sakri loader kada se prikaže rezultat
+        loader.style.display = "none";
+      })
+      .catch((error) => {
+        console.error(error);        
       });
-    })
-    .catch((error) => console.error(error));
+  }, 1000); 
 });
